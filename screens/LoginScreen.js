2f706205e5e8dab270572e83a332984ff1720e8c
Anderson Gabriel <gabriel_13gt@hotmail.com>
2025-06-08 15:03:30 -0300

    changes

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -10,28 +10,32 @@ import {
   KeyboardAvoidingView,
   ScrollView,
   Platform,
-} from 'react-native';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth, db } from '../firebaseConfig';
-import { doc, getDoc } from 'firebase/firestore';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+} from "react-native";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default function LoginScreen({ navigation }) {
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
   const [mostrarSenha, setMostrarSenha] = useState(false);
 
   const handleLogin = async () => {
     if (!email || !senha) {
-      Alert.alert('Erro', 'Preencha todos os campos');
+      Alert.alert("Erro", "Preencha todos os campos");
       return;
     }
 
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, senha);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        senha
+      );
       const user = userCredential.user;
 
-      const userDocRef = doc(db, 'users', user.uid);
+      const userDocRef = doc(db, "users", user.uid);
       const userDocSnap = await getDoc(userDocRef);
 
       if (userDocSnap.exists()) {
@@ -39,47 +43,60 @@ export default function LoginScreen({ navigation }) {
         const tipo = userData.tipo;
 
         switch (tipo) {
-          case 'admin':
-            navigation.replace('Admin');
+          case "admin":
+            navigation.replace("Admin");
             break;
-          case 'fornecedor':
-            navigation.replace('Fornecedor');
+          case "fornecedor":
+            navigation.replace("Fornecedor");
             break;
           default:
-            navigation.replace('Home');
+            navigation.replace("Home");
         }
       } else {
-        Alert.alert('Erro', 'Usuário não encontrado no banco de dados');
+        Alert.alert("Erro", "Usuário não encontrado no banco de dados");
       }
     } catch (error) {
-      Alert.alert('Erro', 'Email ou senha incorretos');
-      setEmail('');
-      setSenha('');
+      Alert.alert("Erro", "Email ou senha incorretos");
+      setEmail("");
+      setSenha("");
     }
   };
 
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={{ flex: 1 }}
     >
-      <ScrollView contentContainerStyle={styles.scrollContainer} keyboardShouldPersistTaps="handled">
+      <ScrollView
+        contentContainerStyle={styles.scrollContainer}
+        keyboardShouldPersistTaps="handled"
+      >
         <View style={styles.container}>
           <Image
-            source={require('../img/mascote.png')} // ajuste o caminho se necessário
+            source={require("../img/logo.png")}
+            style={styles.topImage}
+            resizeMode="contain"
+          />
+          <Image
+            source={require("../img/mascote.png")} // ajuste o caminho se necessário
             style={styles.image}
             resizeMode="contain"
           />
           <Text style={styles.loginTitle}>Login</Text>
           <Text style={styles.signUpText}>
-          Não tem uma conta?
-            <Text style={styles.signUpLink} onPress={() => navigation.navigate('Register')}>
-              {' '}Cadastrar
+            Não tem uma conta?
+            <Text
+              style={styles.signUpLink}
+              onPress={() => navigation.navigate("Register")}
+            >
+              {"  "}
+              Cadastrar
             </Text>
           </Text>
-
+          <Text style={styles.EmailTitle}>Email</Text>
           <TextInput
-            placeholder="Email"
+            placeholder="Ex: Frutigo@gmail.com"
+            placeholderTextColor={"#69A461"}
             style={styles.input}
             value={email}
             onChangeText={setEmail}
@@ -87,9 +104,11 @@ export default function LoginScreen({ navigation }) {
             autoCapitalize="none"
           />
 
+          <Text style={styles.passwordTitle}>Senha</Text>
           <View style={styles.passwordContainer}>
             <TextInput
               placeholder="Senha"
+              placeholderTextColor={"#69A461"}
               style={styles.passwordInput}
               value={senha}
               onChangeText={setSenha}
@@ -97,11 +116,17 @@ export default function LoginScreen({ navigation }) {
               autoCapitalize="none"
             />
             <TouchableOpacity onPress={() => setMostrarSenha(!mostrarSenha)}>
-              <Icon name={mostrarSenha ? 'visibility' : 'visibility-off'} size={24} color="#666" />
+              <Icon
+                name={mostrarSenha ? "visibility" : "visibility-off"}
+                size={24}
+                color="#69A461"
+              />
             </TouchableOpacity>
           </View>
 
-          <TouchableOpacity onPress={() => navigation.navigate('RedefinirSenha')}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("RedefinirSenha")}
+          >
             <Text style={styles.forgotPassword}>Esqueceu a senha ?</Text>
           </TouchableOpacity>
 
@@ -117,73 +142,99 @@ export default function LoginScreen({ navigation }) {
 const styles = StyleSheet.create({
   scrollContainer: {
     flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 35,
+    borderWidth: 1,
+    borderColor: "blue",
   },
   container: {
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "red",
+    marginTop: 20,
   },
+  topImage: {
+    width: 50,
+    height: 40,
+    borderWidth: 1,
+    borderColor: "red",
+    zIndex: 10,
+    position: "absolute",
+    alignSelf: "flex-start",
+  },
+
   image: {
-    width: 180,
-    height: 180,
-    marginBottom: 10,
+    width: 270,
+    height: 300,
+    marginBottom: 0,
+
+    marginTop: 20,
   },
   loginTitle: {
     fontSize: 28,
-    fontWeight: 'bold',
-    marginTop: 10,
+    fontWeight: "bold",
+    marginTop: 0,
+    paddingTop: 0,
   },
   signUpText: {
-    marginTop: 4,
+    marginTop: 10,
     marginBottom: 20,
-    color: '#666',
+    color: "#666",
   },
   signUpLink: {
-    color: '#3478f6',
-    fontWeight: 'bold',
+    color: "#3478f6",
+    fontWeight: "bold",
+  },
+  EmailTitle: {
+    color: "gray",
+    alignSelf: "flex-start",
+  },
+  passwordTitle: {
+    color: "gray",
+    alignSelf: "flex-start",
   },
   input: {
-    width: '100%',
+    width: "100%",
     height: 50,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 1,
     borderRadius: 10,
     paddingHorizontal: 15,
     marginBottom: 15,
   },
   passwordContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderColor: '#ccc',
+    flexDirection: "row",
+    alignItems: "center",
+    borderColor: "#ccc",
     borderWidth: 1,
     borderRadius: 10,
     paddingHorizontal: 15,
     height: 50,
     marginBottom: 10,
-    width: '100%',
+    width: "100%",
   },
   passwordInput: {
     flex: 1,
   },
   forgotPassword: {
-    color: '#1AB65C',
-    textDecorationLine: 'underline',
-    alignSelf: 'flex-end',
+    color: "#69A461",
+    textDecorationLine: "underline",
+    alignSelf: "flex-end",
     marginBottom: 20,
   },
   loginButton: {
-    backgroundColor: '#1AB65C',
-    width: '100%',
+    backgroundColor: "#69A461",
+    width: "100%",
     height: 50,
     borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   loginButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
     fontSize: 16,
   },
 });
